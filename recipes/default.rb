d19b5136ac4cb6bb4bc03b9c1cd1058a573e579f
Christian Townsdin <ctownsdin@expedia.org>
2015-10-22 14:54:32 -0700

    setting up auto members

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,19 +4,13 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = 
-  [{
-    "hostname" => "ec2-54-152-146-21.compute-1.amazonaws.com",
-    "ipaddress" => "54.152.146.21",
-    "port" => 80,
-    "ssl_port" => 80
-  },
-  {
-    "hostname" => "ec2-54-86-162-137.compute-1.amazonaws.com",
-    "ipaddress" => "54.86.162.137",
-    "port" => 80,
-    "ssl_port" => 80
-  }]
+all_web_nodes = search("node", "role:web")
+
+members = []
+
+# TODO: Convert each web node into an array of hashes
+
+node.default['haproxy']['members'] = members
 
 include_recipe "haproxy::default"
 
